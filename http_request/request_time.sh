14e5d839a8c1bbbefcd06ee6881efdddbbd51b7d
zhaohui.eric <zhaohui.eric@bytedance.com>
2018-06-07 14:05:10 +0800

    [add] 添加请求耗时查询

--- http_request/request_time.sh
+++ http_request/request_time.sh
@@ -1,21 +1,42 @@
 #!/bin/sh
 
 # init vars
+OPTIND=1
+usage="usage: $0 -d curl_d -s curl_s\n"
 data=""
 url=""
+format="\
+状态码		%{http_code}\n\
+-----		-----\n\
+DNS解析		%{time_namelookup}s\n\
+连接完成	%{time_appconnect}s\n\
+重定向完成	%{time_redirect}s\n\
+开始传输	%{time_starttransfer}s\n\
+-----		-----\n\
+总时间		%{time_total}s\n\
+"
 
-while getopts 'd:s:' OPT; do
+while getopts :d:s: OPT; do
     case $OPT in
-        d) data=$OPTARG;;
-        s) url=$OPTARG;;
+        d) 
+		data=$OPTARG
+		;;
+        s) 
+		url=$OPTARG
+		;;
+	:)
+		echo $usage
+		exit 1
+		;;
+	?)
+		echo $usage
+		exit 1
+		;;
     esac
 done
 
-echo $data
-echo $url
-
 # do curl
-curl -w "%{time_namelookup}\n" \
+curl -w "${format}\n" \
 -d "$data" \
 -o /dev/null \
 -s "$url"
