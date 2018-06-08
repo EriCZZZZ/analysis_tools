bc2d5646b39757698720d26d00ca2e0452c8fc3b
zhaohui.eric <zhaohui.eric@bytedance.com>
2018-06-08 15:47:16 +0800

    [refactor] clean code

--- load/cpu.sh
+++ load/cpu.sh
@@ -20,10 +20,8 @@ done
 echo "核数"$cpu_core
 
 echo "=========="
-# vmstat
-tmp=`vmstat`
-arr=(等待运行的进程数 1 被阻塞的进程数 2 每秒中断数 11 每秒上下文切换次数 12 用户CPU% 13 系统CPU% 14 空闲CPU% 15 等待IOCPU% 16 虚拟机偷取CPU% 17)
 
+# vmstat
 eval "vmstat 1 2 | awk 'END {printf(\"等待运行的进程数 %d\n\
 被阻塞的进程数 %d\n\
 每秒中断数 %d\n\
@@ -34,5 +32,4 @@ eval "vmstat 1 2 | awk 'END {printf(\"等待运行的进程数 %d\n\
 等待IOcpu %d%%\n\
 VM偷取cpu %d%%\n\
 \", \$1, \$2, \$11, \$12, \$13, \$14, \$15, \$16, \$17)}'"
-
 echo "=========="
